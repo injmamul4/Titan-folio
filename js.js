4d8376154a6c6d81ebaadf8836e9c94e0badbc12
injmamul4 <injmamul1365@gmail.com>
2025-11-10 16:29:27 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("hambtn");
+const menu = document.getElementById("navLinks");
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+
+
